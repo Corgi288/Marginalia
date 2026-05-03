5df24439c5a1604ad8278d36a128f5de2c0e5117
Corgi <serhii2888@gmail.com>
2026-05-03 19:19:47 +0300

    add books and pages

--- marginalia/books/views.py
+++ marginalia/books/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-import requests
+from django.core.paginator import Paginator
 from .models import Books
 
 
 def book_list_view(request):
-    return render(request, 'books/index.html', {'books': Books.objects.all()})
+    books = Books.objects.order_by('?')[:120]
+
+    paginator = Paginator(books, 20)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+    'page_obj': page_obj,
+    }
+
+    return render(request, 'books/index.html', {'page_obj': page_obj})
 
 def books_details(request, book_slug):
     book = get_object_or_404(Books, slug=book_slug)
